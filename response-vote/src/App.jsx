672f9ee531a0215014ee97bf745bd18bb1f28e02
Vijay Daita <vdaita@gmail.com>
2024-05-21 16:09:09 -0500

    removed react-router

--- response-vote/src/App.jsx
+++ response-vote/src/App.jsx
@@ -1,17 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
+import Box from '@mui/joy/Box';
+import { Container } from '@mui/joy';
+import Stack from '@mui/joy/Stack';
+import Button from '@mui/joy/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { app, db } from './utils/firebase';
+import { collection, getDocs, getDoc, doc, FieldValue, increment, setDoc} from 'firebase/firestore';
 
-  return (
-    <>
-      <p>Use the voting page to vote on items.</p>
-    </>
-  )
-}
+export default function Vote() {
+    let [postId, setPostId] = useState("");
+    let [loading, setLoading] = useState(true);
+    let [error, setError] = useState(false);
 
-export default App
+    let [exists, setExists] = useState(true);
+
+    let [body, setBody] = useState("");
+    
+    let [thumbsUp, setThumbsUp] = useState(0)
+    let [thumbsDown, setThumbsDown] = useState(0);
+    
+    let [voted, setVoted] = useState(false);
+
+    useEffect(() => {
+        loadPost();
+    }, []);
+
+    let loadPost = async () => {
+        console.log("starting loadPost");
+        try {
+            const searchParams = new URLSearchParams(window.location.search);
+            const postId = searchParams.get("postId");
+            setPostId(postId);
+            console.log("Post id: ", postId);
+            const postSnap = (await getDoc(doc(db, "posts", postId)));
+            console.log("Post snapshot: ", postSnap);
+            if(!postSnap.exists()){
+                setExists(false);
+            } else {
+                const post = postSnap.data();
+                console.log("Post data: ", post);
+                setBody(post.body);
+                setThumbsUp(post.thumbsUp);
+                setThumbsDown(post.thumbsDown);    
+            }
+            
+            setLoading(false);
+        } catch (e) {
+            console.error(e);
+            setError(true);
+        }
+    }
+
+    let likePost = async () => {
+        let docRef = doc(db, "posts", postId);
+        await setDoc(docRef, {
+            thumbsUp: increment(1)
+        }, {merge: true});
+        setVoted(true);
+    }
+
+    let dislikePost = async () => {
+        let docRef = doc(db, "posts", postId);
+        await setDoc(docRef, {
+            thumbsDown: increment(1)
+        }, {merge: true});
+        setVoted(true);
+    }
+
+    if(error) {
+        return (
+            <Container m={4}>
+                There was an error.
+            </Container>
+        )
+    }
+
+    if(loading) {
+        return (
+            <Container m={4}>
+                Loading...
+            </Container>
+        )
+    }
+
+    if(!exists) {
+        return (
+            <Container m={4}>
+                Post not found.
+            </Container>
+        )
+    }
+
+    if(voted) {
+        return (
+            <Container m={4}>
+                Thank you for voting!
+            </Container>
+        )
+    }
+
+    return (
+        <Container m={4}>
+            <Stack>
+                Voting on post:
+                <br/>
+                {body} <br/> <br/>
+                What did you think about the post: 
+                Current counter: 👍 = {thumbsUp} and 👎 = {thumbsDown}
+                <Button onClick={() => likePost()}>👍</Button>
+                <Button onClick={() => dislikePost()}>👎</Button>
+            </Stack>
+        </Container>
+    )
+};
--- response-vote/src/main.jsx
+++ response-vote/src/main.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Vote from './pages/Vote.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
 import './index.css'
 import '@fontsource/inter';
 
-const router = createBrowserRouter([
- {
-  path: "/",
-  element: <App/>
- },
- {
-  path: "/vote/:postId",
-  element: <Vote/>
- }
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </React.StrictMode>,
 )
